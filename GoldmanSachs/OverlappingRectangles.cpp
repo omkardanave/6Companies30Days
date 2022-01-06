169fbab4063c3aa0b25fa4a09446e439590fdeef
Omkar Danave <85280302+omkardanave@users.noreply.github.com>
2022-01-06 11:41:02 +0530

    Create OverlappingRectangles.cpp

---
+++ GoldmanSachs/OverlappingRectangles.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        // code here
+        return min(R1[0], R2[0]) >= max(L1[0], L2[0]) && min(L1[1],L2[1])>= max(R1[1], R2[1]);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int p[2], q[2], r[2], s[2];
+        cin >> p[0] >> p[1] >> q[0] >> q[1] >> r[0] >> r[1] >> s[0] >> s[1];
+        Solution ob;
+        int ans = ob.doOverlap(p, q, r, s);
+        cout << ans << "\n";
+    }
+}
