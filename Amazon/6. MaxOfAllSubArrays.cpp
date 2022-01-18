e956b6b077c3cd6191581900baae2b65c0b05c02
Omkar Danave <85280302+omkardanave@users.noreply.github.com>
2022-01-18 11:12:26 +0530

    Create 6. MaxOfAllSubArrays.cpp

---
+++ Amazon/6. MaxOfAllSubArrays.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+        vector<int> v;
+        multiset<int, greater<int>> ms;
+        for(int i=0; i<k; i++)
+        {
+            ms.insert(arr[i]);
+        }
+        v.push_back(*ms.begin());
+        for(int i=k; i<n; i++)
+        {
+            ms.erase(ms.lower_bound(arr[i-k]));
+            ms.insert(arr[i]);
+            v.push_back(*ms.begin());
+        }
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i = 0;i<n;i++) 
+	        cin >> arr[i];
+	    Solution ob;
+	    vector <int> res = ob.max_of_subarrays(arr, n, k);
+	    for (int i = 0; i < res.size (); i++) 
+	        cout << res[i] << " ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
